5259be93bf2e1a8bef4002cbbac312cbd23c3790
email4prasanth <email4prasanth@gmail.com>
2026-06-18 12:10:44 +0530

    added qa

--- Backend_Infra/local.tf
+++ Backend_Infra/local.tf
@@ -14,6 +14,11 @@ locals {
       "dev_qa"        = "aitest dev QA"
       "dev_developer" = "aitest dev Developer"
       "dev_manager"   = "aitest dev Manager"
+    },
+    "qa" = {
+      "qa_qa"        = "aitest dev QA"
+      "qa_developer" = "aitest dev Developer"
+      "qa_manager"   = "aitest dev Manager"
     }
   }
 
@@ -24,23 +29,43 @@ locals {
       "dev_qa"        = { role = ["Reader"] },
       "dev_developer" = { role = ["Reader", "AcrPull", "AcrPush", "Virtual Machine User Login", "Storage Blob Data Reader", "Key Vault Secrets User"] } #USER
       "dev_manager"   = { role = ["Reader"] }
+    },
+    "qa" = {
+      "qa_qa"        = { role = ["Reader"] },
+      "qa_developer" = { role = ["Reader", "AcrPull", "AcrPush", "Virtual Machine User Login", "Storage Blob Data Reader", "Key Vault Secrets User"] } #USER
+      "qa_manager"   = { role = ["Reader"] }
     }
   }
   # Define the CIDR ranges for each environment
   cidr_ranges = {
     "dev" = "10.101.0.0/16"
+    "qa" = "10.102.0.0/16"
   }
   vnet_cidr = lookup(local.cidr_ranges, terraform.workspace)
 
   # Define the Key vault name
   key_vault_name = {
-    dev = "${terraform.workspace}${local.project_name.name}kv"
+    dev = "${terraform.workspace}${local.project_name.name}kv",
+    qa = "${terraform.workspace}${local.project_name.name}kv"
   }
 
 
   # Define the Backend VM's security group rules for each environment
   backend_security_group_rules = {
     "dev" = [
+      {
+        name                       = "allow-all"
+        priority                   = 100
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "*"
+        source_port_range          = "*"
+        destination_port_range     = "*"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+      }
+    ]
+    "qa" = [
       {
         name                       = "allow-all"
         priority                   = 100
@@ -63,6 +88,13 @@ locals {
         os_disk_size_gb = 128
         os_disk_type    = "StandardSSD_LRS"
       }
+    },
+    "qa" = {
+      "backend" = {
+        instance_type   = "Standard_B4as_v2"
+        os_disk_size_gb = 128
+        os_disk_type    = "StandardSSD_LRS"
+      }
     }
   }
   # Helper variables for easier access
@@ -71,6 +103,10 @@ locals {
   # Blob Storage Configuration
   blob_storage_config = {
     "dev" = {
+      account_replication_type = "LRS"
+      access_tier              = "Cool"
+    },
+    "qa" = {
       account_replication_type = "LRS"
       access_tier              = "Cool"
     }
@@ -93,9 +129,21 @@ locals {
         storage_tier = "P6"
         storage_mb   = 65536 # 64 GiB
       }
-      org_database_service = {
-        storage_tier = "P10"
-        storage_mb   = 131072 # 128 GiB
+    },
+    qa = {
+      server_name        = "${terraform.workspace}-${local.project_name.name}-psqlserver-centralus"
+      location           = "centralus"
+      sku_name           = "B_Standard_B1ms"
+      database_name      = local.project_name.name
+      database_collation = "en_US.utf8"
+      database_charset   = "UTF8"
+      firewall_rule_name = "AllowAll"
+      firewall_start_ip  = "0.0.0.0"
+      firewall_end_ip    = "255.255.255.255"
+
+      main_database_service = {
+        storage_tier = "P6"
+        storage_mb   = 65536 # 64 GiB
       }
     }
   }
